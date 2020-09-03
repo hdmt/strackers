4cebbd17764fe883ecda1516306a591924ddcdd9
hdmt <tanaka@trsp.biz>
2020-09-03 09:50:31 +0900

    update

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          test
+        </p>
+      </header>
+    </div>
+  );
+}
+
+
+
+export default App;
